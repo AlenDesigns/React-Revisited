6211707a6848edf23f77937ecfb4fbdfe51bf380
Alen Mehic <alen@empireweb,edia.co.uk>
2019-12-01 23:58:40 +0000

    Exploring React Hooks and Effects!

---
+++ src/SearchParams.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  // Hooks never go inside if statements!
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // Pass [] for running only once!
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+            placeholder="location"
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return <h1>Hello</h1>;
+  return (
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt me</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
+  );
 };
 
 render(<App />, document.getElementById("root"));
